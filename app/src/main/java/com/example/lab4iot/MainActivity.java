8ea275aa806e5ec85539f10e91b56f34839055d8
Minh PK <minh.phambku51@hcmut.edu.vn>
2024-04-14 20:55:45 +0700

    first commit

---
+++ app/src/main/java/com/example/lab4iot/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lab4iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
